4c1dc8ff1c16d6972f03bc5260367b057e2a02ee
Manishshriwas <manishshriwasjnvmahoba@gmail.com>
2025-08-21 01:05:38 +0530

    Initial commit: FastAI client application

---
+++ src/components/pages/GenerateImages.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GenerateImages = () => {
+  return (
+    <div>
+      <h1>GenereteImage</h1>
+    </div>
+  )
+}
+
+export default GenerateImages
---
+++ src/components/pages/RemoveBackground.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RemoveBackground = () => {
+  return (
+    <div>
+      <h1>RemoveObject</h1>
+    </div>
+  )
+}
+
+export default RemoveBackground
